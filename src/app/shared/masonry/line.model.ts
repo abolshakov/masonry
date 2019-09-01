166fe010d75f64562087b2ac51d5ce2bb3fe4ef0
GLOBALLOGIC / OLEKSANDR BOLSHAKOV <oleksandr.bolshakov@globallogic.com>
2019-09-01 23:30:08 +0300

    Temporal changes

--- src/app/shared/masonry/line.model.ts
+++ src/app/shared/masonry/line.model.ts
@@ -3,59 +3,50 @@ import { RelativeSize } from './relative-size.model';
 
 export class Line {
     private _elements: ElementInfo[] = [];
-    // private _freeSpace: number;
-
-    public get freeSpace(): number {
-        // console.log('FREE',
-        //  this.size.mainAxis,
-        //  this._elements,
-        //  this.size.crossAxisName,
-        //  this._elements.reduce((prev, next) => prev[this.size.crossAxisName] + next[this.size.crossAxisName], 0),
-        //   this.size.mainAxis - this._elements.reduce((prev, next) => prev[this.size.crossAxisName] + next[this.size.crossAxisName], 0));
-        const free = this.size.mainAxis - this._elements.map(x => x[this.size.crossAxisName]).reduce((prev, next) => prev + next, 0);
-        //console.log('FREE', free);
-        return free;
-        // return this._freeSpace;
-    }
+    private _freeSpace: number;
 
     public get elements(): ElementInfo[] {
         return this._elements;
     }
 
+    public get freeSpace(): number {
+        return this._freeSpace;
+    }
+
     public constructor(private size: RelativeSize) {
-        // this._freeSpace = this.size.mainAxis;
+        this._freeSpace = this.size.mainAxis;
     }
 
     public assign(element: ElementInfo): boolean {
-        if (this.freeSpace <= 0) {
+        if (this._freeSpace <= 0) {
             return false;
         }
         const clone = element.clone();
         clone[this.size.crossAxisName] = this.size.crossAxis;
         const size = new RelativeSize(clone.width, clone.height, this.size.direction);
-        const remainingSpace = this.freeSpace - size.mainAxis;
+        const remainingSpace = this._freeSpace - size.mainAxis;
         if (remainingSpace < 0) {
             if (this._elements.length === 0) {
                 clone[this.size.mainAxisName] = this.size.mainAxis;
                 this._elements.push(clone);
-                // this.freeSpace = 0;
+                this._freeSpace = 0;
                 return true;
             }
             return false;
         }
         this._elements.push(clone);
-        // this._freeSpace = remainingSpace;
+        this._freeSpace = remainingSpace;
         return true;
     }
 
     public fit() {
-        console.log('BEFORE FIT', this.freeSpace);
-        if (!this.freeSpace) {
+        console.log('BEFORE FIT', this._freeSpace);
+        if (!this._freeSpace) {
             return;
         }
-        const ratio = (this.size.mainAxis - this.freeSpace) / this.size.crossAxis;
+        const ratio = (this.size.mainAxis - this._freeSpace) / this.size.crossAxis;
         const cross = this.size.mainAxis / ratio;
         this.elements.forEach(x => x[this.size.crossAxisName] = cross);
-        console.log('FIT', this.freeSpace);
+        console.log('FIT', this.size.width - this._elements.map(x => x.width).reduce((prev, curr) => prev + curr, 0));
     }
 }
